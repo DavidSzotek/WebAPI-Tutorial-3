ef47054a0bddf34f4dcf1cb516182e4256d1837f
David Szotek <david.szotek.vs@gmail.com>
2024-01-10 13:29:49 +0100

    Setting up the project
    -Created the project in .NET 8 by using Visual Studio's template
    -Deleted default model and controller
    -Installed NuGet packages:
          -Microsoft.EntityFrameworkCore
          -Microsoft.EntityFrameworkCore.Tools
          -Microsoft.EntityFrameworkCore.Design
          -Microsoft.EntityFrameworkCore.SqlServer
    -Created DbContext class "DataContext.cs"
    -Registered DbContext service

---
+++ WebAPI-Tutorial-3/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI_Tutorial_3.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+    }
+}
